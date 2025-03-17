80c5e99463b114166c12c5c5ed42e31ece341866
haneul <68280891+bbongsky11@users.noreply.github.com>
2025-03-17 09:32:03 +0900

    [Bronze I] Title: 소인수분해, Time: 136 ms, Memory: 14684 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        while (n != 1) {
+            for (int i = 2; i <= n; i++) {
+                if (n % i == 0) {
+                    System.out.println(i);
+                    n = n / i ;
+                    break;
+                }
+            }
+        }
+    }
+}

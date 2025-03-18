6fbd2657b841d9aa7cba56a07ab0c73fff64dc40
haneul <68280891+bbongsky11@users.noreply.github.com>
2025-03-18 10:08:11 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT ai.ANIMAL_ID,	ai.ANIMAL_TYPE,	ai.NAME
+from ANIMAL_INS ai
+inner join ANIMAL_OUTS ao
+on ai.ANIMAL_ID = ao.ANIMAL_ID
+where ai.SEX_UPON_INTAKE != ao.SEX_UPON_OUTCOME

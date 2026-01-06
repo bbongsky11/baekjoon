8a66a34756d87f2093949984b3cb2e782f2ea3f2
haneul <68280891+bbongsky11@users.noreply.github.com>
2026-01-06 10:30:41 +0900

    [level 0] Title: 더 크게 합치기, Time: 12.09 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        String sum1 = a + "" + b;
+        String sum2 = b + "" + a;
+        if(Integer.parseInt(sum1) > Integer.parseInt(sum2)) {
+            answer = Integer.parseInt(sum1);
+        } else {
+            answer = Integer.parseInt(sum2);
+        }
+        
+        System.out.println(answer);
+        return answer;
+    }
+}

1cea6e1dd9cde73609dfb82f9a92ed7d12d4222a
haneul <68280891+bbongsky11@users.noreply.github.com>
2026-01-14 18:48:23 +0900

    [level 2] Title: 가장 큰 수, Time: 180.14 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,6 +2,7 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String[] answer = new String[numbers.length];
+        StringBuilder sb = new StringBuilder();
         
         for(int i = 0; i < numbers.length; i++) {
             answer[i] = String.valueOf(numbers[i]);
@@ -9,8 +10,12 @@ class Solution {
         
         Arrays.sort(answer, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
         
+        for(String s : answer) {
+            sb.append(s);
+        }
+        
         if("0".equals(answer[0])) return "0";
         
-        return String.join("", answer);
+        return sb.toString();
     }
 }
